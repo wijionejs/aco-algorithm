dc49cbfdb0b96d2a629889398b06efd7b6e9f17b
Volodymyr Baranchuk <volodymyr.baranchuk@teladochealth.com>
2024-09-17 10:50:07 +0300

    update config

--- ACOAlgorithm.js
+++ ACOAlgorithm.js
@@ -11,15 +11,15 @@ class ACOAlgorithm {
 
   constructor(graph) {
     this.graph = graph;
-    const antsNumber = graph.cities.length;
-    const DEFAULT_PHEROMONE_PER_ANT = config.DEFAULT_PHEROMONE / antsNumber;
+    const antsNumber = config.ANTS_NUMBER || graph.cities.length;
+    const INITIAL_PHEROMONE_PER_ANT = config.INITIAL_PHEROMONE / antsNumber;
   
-    this.pheromoneMatrix = new PheromoneMatrix(graph, DEFAULT_PHEROMONE_PER_ANT, config.PHEROMONE_DEPOSIT, config.EVAPORATION_RATE);
+    this.pheromoneMatrix = new PheromoneMatrix(graph, INITIAL_PHEROMONE_PER_ANT, config.PHEROMONE_DEPOSIT, config.EVAPORATION_RATE);
     this.antColony = new AntColony(graph, this.pheromoneMatrix, antsNumber, config.ALPHA, config.BETA);
   }
 
   checkIsFinished() {
-    this.isFinished = this.iterationsWithoutChange >= config.ITERATIONS_WITHOUT_CHANGE_LIMIT;
+    this.isFinished = this.iterationsWithoutChange >= config.ITERATIONS_WITHOUT_CHANGE_LIMIT * this.graph.cities.length;
   }
 
   iterate() {
--- config.js
+++ config.js
@@ -1,8 +1,9 @@
 export default {
-  ITERATIONS_WITHOUT_CHANGE_LIMIT: 100,
+  ITERATIONS_WITHOUT_CHANGE_LIMIT: 20, // Multiplied by the number of cities 
   ALPHA: 1,
   BETA: 3,
-  DEFAULT_PHEROMONE: 10, // Will be divided evenly between the ants
+  // ANTS_NUMBER: 10, // Optional. If omitted, will be equal to the number of cities
+  INITIAL_PHEROMONE: 10, // Will be divided evenly between the ants
   PHEROMONE_DEPOSIT: 10,
   EVAPORATION_RATE: 0.1
 }
